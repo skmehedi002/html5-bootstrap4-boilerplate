5a5e30d3db02a4638c12e0623c11c3f9eacea660
Sk Mehedi Hasan <mehedi.cse.002@gmail.com>
2020-08-20 16:55:22 +0600

    Update app-jquery.js

--- assets/js/app-jquery.js
+++ assets/js/app-jquery.js
@@ -52,10 +52,36 @@
 
         WebSite.prototype.initTestimonialSlider = function() {
             $('#owl-demo').owlCarousel({
-                items: 1,
                 autoplay: true,
-                itemsDesktop: [1199, 1],
-                itemsDesktopSmall: [979, 1]
+                lazyLoad: true,
+                loop: true,
+                margin: 20,
+                /*
+               animateOut: 'fadeOut',
+               animateIn: 'fadeIn',
+               */
+                responsiveClass: true,
+                autoHeight: true,
+                autoplayTimeout: 7000,
+                smartSpeed: 800,
+                nav: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+
+                    600: {
+                        items: 3
+                    },
+
+                    1024: {
+                        items: 4
+                    },
+
+                    1366: {
+                        items: 4
+                    }
+                }
             });
         },
 
